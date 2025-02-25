b3f594d72f56982847903f6f3bd32dd8c5073ff0
emiltervo <73493921+emiltervo@users.noreply.github.com>
2025-02-25 15:23:14 +0100

    Added BoilerPlate needed for Labb3

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { projectRouter } from "./router/project-router";
 import { userRouter } from "./router/user-router";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/projects", projectRouter);
 app.use("/users", userRouter);
